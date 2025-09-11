9243eea50b358f01cdf2f0be533470c8fc8e4812
AK <akrivit@gmail.com>
2025-09-11 15:44:55 -0400

    add CI

--- homebrew-tap/Formula/duck-shard.rb
+++ homebrew-tap/Formula/duck-shard.rb
@@ -1,12 +1,13 @@
 class DuckShard < Formula
   desc "Fast, cross-platform CLI tool to convert Parquet, CSV, NDJSON, JSON with DuckDB"
   homepage "https://github.com/ak--47/duck-shard"
-  url "https://raw.githubusercontent.com/ak--47/duck-shard/main/duck-shard.sh"
-  version "main"
-  sha256 "2e37fe9185806c55b5ee4bfa90b57a9b1dad13a262a89864d2668d2025d09b58" # Will be set by GitHub Action!
+  url "https://raw.githubusercontent.com/ak--47/duck-shard/v1.1.0/duck-shard.sh"
+  version "1.1.0"
+  sha256 "2e37fe9185806c55b5ee4bfa90b57a9b1dad13a262a89864d2668d2025d09b58"
   license "MIT"
 
   depends_on "duckdb"
+  depends_on "node", optional: true
 
   def install
     bin.install "duck-shard.sh" => "duck-shard"
